f56e991f1f3f0535a6a03c17791ccb7ba0278bc4
forward0606 <chen910606@gmail.com>
2022-09-30 02:11:25 +0800

    add EXT

--- src/Algorithm/QCAST/QCAST.cpp
+++ src/Algorithm/QCAST/QCAST.cpp
@@ -8,3 +8,26 @@ QCAST::QCAST()
 void QCAST::path_assignment(){
     
 }
+double QCAST::C(n, m){
+    if(n < m){
+        cerr<<"err:\tint QCAST combination m > n"<<endl;
+        exit(1);
+    }
+    if(n == m || m == 0){return 1;}
+    if(combination.find(get_pair(n, m)) != combination.end()){
+        return combination[get_pair(n, m)];
+    }
+    combination[get_pair(n, m)] = C(n-1, m) + C(n-1, m-1);
+    return combination[get_pair(n, m)];
+}
+double QCAST::EXT(vector<int> path, int w){
+    vector<vector<double>> q(path.size()); //q[i][j] the prob of j links success ith edge on path 
+    for(int i=0;i<q.size();i++){
+        q[i].resize(w)+1;
+    }
+    for(int k=0;k<q.size();k++){
+        for(int i=0;i<=w;i++){
+            q[k][i] = C(w, i) * pow(path[k].prob, i) * pow(1-path[k].prob, w-i);
+        }
+    }
+}
--- src/Algorithm/QCAST/QCAST.h
+++ src/Algorithm/QCAST/QCAST.h
@@ -1,8 +1,12 @@
 #include    <iostream>
+#include    <cmath>
 #include    "../AlgorithmBase/AlgorithmBase.h"
 using namespace std;
 
 class QCAST:public AlgorithmBase{
+    double EXT(vector<int> path, int w);
+    map<pair<int, int>, double> combination;
+    double C(n, m);
 public:
     QCAST();
     void path_assignment();
