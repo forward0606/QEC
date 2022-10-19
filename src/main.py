0d927f083feeb17fb180da4f1e2e65bf39275a29
forward0606 <chen910606@gmail.com>
2022-10-19 16:50:44 +0800

    new start

--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ if  len(sys.argv) <= 1:
 
 
 filename = sys.argv[1]
-num_of_node = 500
+num_of_node = 5
 min_channel_cnt = 2
 max_channel_cnt = 10
 min_memory_cnt = 3
@@ -89,8 +89,26 @@ for n in G.nodes():
     (p1, p2) = positions[n]
     print(str(p1 * 20) + ' ' + str(p2 * 20) + " " + str(ceil(random.random()*(max_memory_cnt-min_memory_cnt)) + min_memory_cnt), file=f)
 
+edge_num = 0
+for e in G.edges():
+    if e[0] != e[1]:#file 
+        edge_num += 1
+print(edge_num, file=f)
 for e in G.edges():
     if e[0] != e[1]:#file 
         print(str(e[0]) + ' ' + str(e[1]) + " " + str(ceil(random.random()*(max_channel_cnt-min_channel_cnt)) + min_channel_cnt), file=f)
+
+for i in range(num_of_node):
+    for j in range(num_of_node):
+        if i == j:
+            print(1, end=" ", file=f)
+        else:
+            if random.random() > 0.5:
+                print(1, end=" ", file=f)
+            else:
+                print(0, end=" ", file=f)
+    print("", file=f)
+
 print("----------graph generate finished!----------", file=sys.stderr)
 print("----------graph generate finished!----------")
+
