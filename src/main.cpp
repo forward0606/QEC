69f082fa1340b3eb54216fdb0c415baeb8cf2f38
forward0606 <chen910606@gmail.com>
2022-10-28 23:49:28 +0800

    adjust parameter

--- src/main.cpp
+++ src/main.cpp
@@ -55,13 +55,12 @@ int main(){
     default_setting["service_time"] = 10;
 
     map<string, vector<double>> change_parameter;
-    change_parameter["swap_prob"] = {0.1, 0.3, 0.5, 0.7, 0.9, 1};
-    change_parameter["entangle_alpha"] = {0.02, 0.002, 0};
+    change_parameter["swap_prob"] = {0.3, 0.5, 0.7, 0.9, 1};
+    change_parameter["entangle_alpha"] = {0.02, 0.002, 0.0002, 0};
     change_parameter["min_fidelity"] = {0.5, 0.75, 0.85, 0.95, 0.99};
     change_parameter["resource_ratio"] = {0.5, 1, 2, 10}; 
     change_parameter["service_time"] = {1, 5, 10};
-    // change_parameter["area"] = {1, 10, 100, 1000}; 
-    change_parameter["area_alpha"] = {0.01, 0.05, 0.1}; 
+    change_parameter["area_alpha"] = {0.001, 0.01, 0.1}; 
     change_parameter["social_density"] = {0.25, 0.5, 0.75, 1}; 
 
     vector<string> X_names = {"area_alpha", "resource_ratio", "social_density", "min_fidelity", "swap_prob",  "entangle_alpha", "service_time"};
@@ -205,7 +204,7 @@ int main(){
             for(string algo_name : algo_names){
                 for(int T = 0; T < round; T++){
                     result[T][algo_name]["waiting_time"] /= result[T][algo_name]["total_request"];
-                    result[T][algo_name]["succ-finished_rate(1)"] = result[T][algo_name]["throughputs"] / result[T][algo_name]["finished_throughputs"];
+                    result[T][algo_name]["succ-finished_rate"] = result[T][algo_name]["throughputs"] / result[T][algo_name]["finished_throughputs"];
                     result[T][algo_name]["path_length"] = result[T][algo_name]["path_length"] / result[T][algo_name]["finished_throughputs"];
                 }
             }
@@ -217,14 +216,10 @@ int main(){
                 ofs << change_value << ' ';
                 
                 for(string algo_name : algo_names){
-                    if(Y_name == "succ-finished_rate(0)"){
-                        ofs << sum_res[algo_name][Y_name] << ' ';
-                    }else{
-                        for(int T = 0; T < round; T++){
-                            sum_res[algo_name][Y_name] += result[T][algo_name][Y_name];
-                        }
-                        ofs << sum_res[algo_name][Y_name] / round << ' ';
+                    for(int T = 0; T < round; T++){
+                        sum_res[algo_name][Y_name] += result[T][algo_name][Y_name];
                     }
+                    ofs << sum_res[algo_name][Y_name] / round << ' ';
                 }
                 ofs << endl;
                 ofs.close();
