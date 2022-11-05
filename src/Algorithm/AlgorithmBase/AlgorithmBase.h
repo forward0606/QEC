bd6e83a7a9934557810b31a0bae157106660fe5d
forward0606 <chen910606@gmail.com>
2022-11-06 02:33:46 +0800

    receive request

--- src/Algorithm/AlgorithmBase/AlgorithmBase.h
+++ src/Algorithm/AlgorithmBase/AlgorithmBase.h
@@ -39,6 +39,7 @@ public:
     void base_send();
     void base_next_time_slot();
     void base_test_active();
+    //void request_received();
     void check_resource();
     virtual void entangle()=0;
     virtual void swap()=0;
